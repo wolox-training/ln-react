4a9e4c81d3f2ff97902fdf254c81cdd2b68028cc
lucasnestrojil <lucasnestrojil@gmail.com>
2019-04-05 12:24:32 -0300

    Added state change functionality to Square component

--- react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -5,8 +5,18 @@ import Square from '../Square';
 import styles from './styles.module.scss';
 
 class Board extends Component {
-  renderSquare() {
-    return <Square />;
+  state = { squares: Array(9).fill(null) }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
+  renderSquare(i) {
+    return <Square 
+             value={this.state.squares[i]}
+             onClick={() => this.handleClick(i)} />;
   }
 
   render() {
--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 
-const Square = () => (
-  <button type="button" className={styles.square}>
-    {/* TODO */}
-  </button>
-);
+const Square = (props) => {
+  return (
+    <button 
+        type="button" 
+        className={styles.square}
+        onClick={props.onClick} >
+          {props.value}
+    </button>
+  );
+}
+
+Square.propTypes = {
+  value: PropTypes.number
+};
 
 export default Square;
