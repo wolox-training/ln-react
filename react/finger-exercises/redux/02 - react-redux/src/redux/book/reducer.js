15b53bde2fde73d2c60956662d5d8430c1365cf1
lucasnestrojil <lucasnestrojil@gmail.com>
2019-04-10 13:55:34 -0300

    Redux implemented into finger exercises

--- react/finger-exercises/redux/02 - react-redux/src/redux/book/reducer.js
+++ react/finger-exercises/redux/02 - react-redux/src/redux/book/reducer.js
@@ -9,15 +9,38 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case actions.GET_BOOKS: // TODO to implement the logic
-      return { ...state };
+      return {
+        ...state,
+        books: action.payload,
+        originalData: action.payload
+      };
     case actions.ADD_TO_CART: // TODO to implement the logic
-      return { ...state };
+      return {
+        ...state,
+        bookSelected: state.bookSelected.concat(action.payload) // [...state, action.payload]
+      };
     case actions.ADD_ITEM: // TODO to implement the logic
-      return { ...state };
+      return {
+        ...state,
+        bookSelected: state.bookSelected.map(obj => {
+          if (obj.id === action.payload) {
+            obj.quantity += 1;
+          }
+          return obj;
+        })
+      };
     case actions.REMOVE_ITEM: // TODO to implement the logic
-      return { ...state };
+      return {
+        ...state,
+        bookSelected: state.bookSelected.filter(item => item.id !== action.payload)
+      };
     case actions.SEARCH_ITEM: // TODO to implement the logic
-      return { ...state };
+      return {
+        ...state,
+        books: state.originalData.filter(item =>
+          item.name.toLowerCase().includes(action.payload.toLowerCase())
+        )
+      };
     default:
       return state;
   }
